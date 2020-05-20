7e80a3c7a42fc91cf187461d6284e126a7ce10b1
Harold Lee <harry.yg.lee@gmail.com>
2020-05-20 18:33:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function theBeatlesPlay(musicians,instruments){
+  let array=[]
+  for(let i=0;i<musicians.length;i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+function johnLennonFacts(facts){
+let i = 0
+while(i<johnLennonFacts.length)
+facts[i]+='!!!'
+return array
+}
 // add solution here
